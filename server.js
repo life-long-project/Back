1292bc38e1f4abe33cc5ecbc224c6a55f3b353ec
Mahmoud Mosbah <mahmodmosbah50@gmail.com>
2023-05-14 20:24:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const nodemon = require("nodemon");
 // Mongo db
 mongoose.set("strictQuery", false);
 mongoose.connect(
-  process.env.DB_URL
-  //"mongodb+srv://xmosb7:xmosb7@cluster0.hpsckra.mongodb.net/shaghlny?retryWrites=true&w=majority"
+    process.env.DB_URL
+    //"mongodb+srv://xmosb7:xmosb7@cluster0.hpsckra.mongodb.net/shaghlny?retryWrites=true&w=majority"
 );
 const db = mongoose.connection;
 db.on("error", (error) => console.error("Error while connecting to mongodb " + error));
@@ -33,16 +33,16 @@ app.use(cors());
 
 // middleware for OPTIONS method
 app.use((req, res, next) => {
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-    return res.status(200).json();
-  }
-  next();
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+        return res.status(200).json();
+    }
+    next();
 });
 
 //json middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
 // authentication routes
 require("./auth/auth");
@@ -55,7 +55,7 @@ app.use('/user', passport.authenticate('jwt', {session: false}), secureRoute)
 
 //admin routes
 const adminRoute = require('./routes/admin/admin_router');
-app.use('/admin', passport.authenticate('jwt', {session: false}),adminRoute);
+app.use('/admin', passport.authenticate('jwt', {session: false}), adminRoute);
 
 //rate routes
 const rateRoute = require('./routes/secure/rate_router');
@@ -69,36 +69,36 @@ app.use('/offer', passport.authenticate('jwt', {session: false}), offerRoute);
 // image upload
 // todo: make a middleware for upload profile image and id and job post images
 app.post(
-  "/upload",
-  passport.authenticate("jwt", { session: false }),
-  upload,
-  (req, res) => {
-    const files = req.files;
-    const responses = [];
-    const f_response = [];
+    "/upload",
+    passport.authenticate("jwt", {session: false}),
+    upload,
+    (req, res) => {
+        const files = req.files;
+        const responses = [];
+        const f_response = [];
 
-    // Upload images to Cloudinary
-    // todo: can make this function just return the url as img_url in the req to continue your proccess
-    files.forEach((file) => {
-      uploadImage(file, (err, result) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).send(err);
-        }
-        responses.push(result);
-        if (responses.length === files.length) {
-          responses.forEach((resp) => {
-            f_response.push({
-              url: resp.secure_url,
-              public_id: resp.public_id,
-              format: resp.format,
+        // Upload images to Cloudinary
+        // todo: can make this function just return the url as img_url in the req to continue your proccess
+        files.forEach((file) => {
+            uploadImage(file, (err, result) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send(err);
+                }
+                responses.push(result);
+                if (responses.length === files.length) {
+                    responses.forEach((resp) => {
+                        f_response.push({
+                            url: resp.secure_url,
+                            public_id: resp.public_id,
+                            format: resp.format,
+                        });
+                    });
+                    return res.send(f_response);
+                }
             });
-          });
-          return res.send(f_response);
-        }
-      });
-    });
-  }
+        });
+    }
 );
 
 
@@ -112,7 +112,7 @@ app.use("/jobs", job_post_router);
 
 //profiles
 const profile_router = require("./routes/public/profileRoutes");
-const { response } = require("express");
+const {response} = require("express");
 app.use("/profile", profile_router);
 
 const messenger_router = require("./routes/public/messeges");
@@ -122,11 +122,9 @@ const conversation_router = require("./routes/public/conversation");
 app.use("/conversation", conversation_router);
 
 
-
-
 // deploy server
 const port = process.env.PORT || 3000;
 const expressServer = app.listen(port, () =>
-  console.log(`server had started on port: ${port}`)
+    console.log(`server had started on port: ${port}`)
 );
 const io = socketio(expressServer);
