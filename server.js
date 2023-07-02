20366a670c977a3740b5a77ea38b7dde21166045
mayman5525 <>
2023-07-02 22:56:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -135,31 +135,9 @@ const expressServer = app.listen(port, () =>
 );
 const io = socketio(expressServer);
 
-let users = [];
-
-const addUser = (userId, socketId) => {
-  !users.some((user) => user.userId === userId) &&
-    users.push({ userId, socketId });
-};
-
-const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId);
-};
-
-const getUser = (userId) => {
-  return users.find((user) => user.userId === userId);
-};
-
 io.on("connection", (socket) => {
   //when ceonnect
   console.log("a user connected." + socket.id);
-
-  //take userId and socketId from user
-  socket.on("addUser", (userId) => {
-    console.log(userId, socket.id);
-    addUser(userId, socket.id);
-    io.emit("getUsers", users);
-  });
 
   //send and get message
   socket.on("sendMessage", ({ senderId, receiverId, text }) => {
